0127482a9208199255224a6893ae383047a7bb58
JenJost <jeniferjost@me.com>
2015-10-23 16:43:36 -0500

    Create GpaCalculator.java

---
+++ GpaCalculator.java
@@ -0,0 +1,12 @@
+import java.util.Scanner; // this allows users to enter information
+import java.text.DecimalFormat; // this will help format the GPA in 0.00 format
+
+public class GpaCalculator {  
+
+    public static void main(final String[] args) {
+    
+    
+    
+    }
+}
+
